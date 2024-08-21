b0692e2e66065bfd6625119a1262143e605424b7
huseynovvusal <vslh333222111@gmail.com>
2024-08-21 10:53:50 +0400

    Get Answers of Question

--- src/controllers/asnwer.controller.ts
+++ src/controllers/asnwer.controller.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express"
 import asyncErrorWrapper from "express-async-handler"
 import Answer from "../models/answer.model"
 import CustomError from "../helpers/error/CustomError"
+import Question from "../models/question.model"
+import { IQuestion } from "../interfaces/question"
 
 export const addNewAnswerToQuestion = asyncErrorWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -20,6 +22,24 @@ export const addNewAnswerToQuestion = asyncErrorWrapper(
     res.status(200).json({
       success: true,
       data: answer,
+    })
+  }
+)
+
+export const getAllAnswersByQuestion = asyncErrorWrapper(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { questionId } = req.params
+
+    const question = (await Question.findById(questionId).populate(
+      "answers"
+    )) as IQuestion
+
+    const answers = question.answers
+
+    res.status(200).json({
+      success: true,
+      data: answers,
+      count: answers.length,
     })
   }
 )
--- src/routers/answer.router.ts
+++ src/routers/answer.router.ts
@@ -1,9 +1,13 @@
 import express from "express"
-import { addNewAnswerToQuestion } from "../controllers/asnwer.controller"
+import {
+  addNewAnswerToQuestion,
+  getAllAnswersByQuestion,
+} from "../controllers/asnwer.controller"
 import authenticateToken from "../middlewares/auth/authenticateToken"
 
 const router = express.Router({ mergeParams: true })
 
 router.post("/", authenticateToken, addNewAnswerToQuestion)
+router.get("/", getAllAnswersByQuestion)
 
 export default router
